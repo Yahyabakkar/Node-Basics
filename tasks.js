740daa89d03b62403c76cc0d197eb8ca0979b3c9
yahyabakkar <yahyabakkar022@gmail.com>
2022-12-23 09:06:20 +0200

    exit command

--- tasks.js
+++ tasks.js
@@ -34,7 +34,7 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === 'quit\n' , 'exit') {
     quit();
   }
   else if(text === 'hello\n'){
@@ -74,7 +74,7 @@ function hello(){
  * @returns {void}
  */
 function quit(){
-  console.log('Quitting now, goodbye!')
+  console.log('goodbye!')
   process.exit();
 }
 
